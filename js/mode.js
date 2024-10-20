fffdfbdce59989afde1768da0ef982d67115b874
AngelChira <125518726+AngelChira@users.noreply.github.com>
2024-10-19 23:55:21 -0500

    rename file

---
+++ js/mode.js
@@ -0,0 +1,44 @@
+const cloud = document.querySelector("#logoHeader");
+const barraLateral = document.querySelector(".barra-lateral");
+const spans = document.querySelectorAll("span");
+const palanca = document.querySelector(".switch");
+const circulo = document.querySelector(".circulo");
+const menu = document.querySelector(".menu");
+const main = document.querySelector("main");
+
+// Seleccionar la raiz del documento (:root)
+const root = document.documentElement;
+
+menu.addEventListener("click", () => {
+  barraLateral.classList.toggle("max-barra-lateral");
+  if (barraLateral.classList.contains("max-barra-lateral")) {
+    menu.children[0].style.display = "none";
+    menu.children[1].style.display = "block";
+  } else {
+    menu.children[0].style.display = "block";
+    menu.children[1].style.display = "none";
+  }
+  if (window.innerWidth <= 320) {
+    barraLateral.classList.add("mini-barra-lateral");
+    main.classList.add("min-main");
+    spans.forEach((span) => {
+      span.classList.add("oculto");
+    });
+  }
+});
+
+// Cambiar el color de fondo de la página
+palanca.addEventListener("click", () => {
+  let body = document.querySelector("body");
+  const modo = body.classList.toggle("dark-mode") ? "dark-mode" : "light-mode";
+  localStorage.setItem("theme", modo);
+  circulo.classList.toggle("prendido");
+});
+
+cloud.addEventListener("click", () => {
+  barraLateral.classList.toggle("mini-barra-lateral");
+  main.classList.toggle("min-main");
+  spans.forEach((span) => {
+    span.classList.toggle("oculto");
+  });
+});
