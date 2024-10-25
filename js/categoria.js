5b8f73c33ff5cc17283f23a1ac66cd11ef77cd5f
AngelChira <125518726+AngelChira@users.noreply.github.com>
2024-10-25 00:34:02 -0500

    listar categorias en el html

---
+++ js/categoria.js
@@ -0,0 +1,43 @@
+import { listar } from "../supabase/operaciones.js";
+
+const dataCategorias = await listar("categoria", "idcategoria");
+console.log(dataCategorias);
+
+function mostrarCategorias() {
+  const { data: categorias, error } = dataCategorias;
+  if (error) {
+    alert("Error al cargar las categorias");
+    return;
+  }
+  const tbody = document.querySelector(".table__body");
+  tbody.innerHTML = "";
+  categorias.forEach((categoria) => {
+    const tr = document.createElement("tr");
+    tr.innerHTML = `
+      <td class="table__cell">${categoria.idcategoria}</td>
+        <td class="table__cell">${categoria.nombre}</td>
+        <td class="table__cell">
+        <button class="table__button">
+          <img
+            width="25"
+            height="25"
+            src="https://img.icons8.com/color/48/edit--v1.png"
+            alt="edit--v1"
+          />
+        </button>
+        <button class="table__button">
+          <img
+            width="25"
+            height="25"
+            src="https://img.icons8.com/color/48/delete.png"
+            alt="delete"
+          />
+        </button>
+      </td>
+    `;
+    tr.classList.add("table__row");
+    tbody.appendChild(tr);
+  });
+}
+
+mostrarCategorias();
