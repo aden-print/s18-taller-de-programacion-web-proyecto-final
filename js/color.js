ca2b4ef72aa459d59c403234e55efcce332b1c80
AngelChira <125518726+AngelChira@users.noreply.github.com>
2024-10-25 00:34:37 -0500

    listar los colores en el html con js

---
+++ js/color.js
@@ -0,0 +1,43 @@
+import { listar } from "../supabase/operaciones.js";
+
+const dataColores = await listar("color", "idcolor");
+console.log(dataColores);
+
+function mostrarColores() {
+  const { data: colores, error } = dataColores;
+  if (error) {
+    alert("Error al cargar los colores");
+    return;
+  }
+  const tbody = document.querySelector(".table__body");
+  tbody.innerHTML = "";
+  colores.forEach((color) => {
+    const tr = document.createElement("tr");
+    tr.innerHTML = `
+      <td class="table__cell">${color.idcolor}</td>
+        <td class="table__cell">${color.nombre}</td>
+        <td class="table__cell">
+        <button class="table__button">
+          <img
+            width="25"
+            height="25"
+            src="https://img.icons8.com/color/48/edit--v1.png"
+            alt="edit--v1"
+          />
+        </button>
+        <button class="table__button">
+          <img
+            width="25"
+            height="25"
+            src="https://img.icons8.com/color/48/delete.png"
+            alt="delete"
+          />
+        </button>
+      </td>
+    `;
+    tr.classList.add("table__row");
+    tbody.appendChild(tr);
+  });
+}
+
+mostrarColores();
