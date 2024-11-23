f13ab189661a7f4f6a38a6d82017008b2b2fb90d
AngelChira <125518726+AngelChira@users.noreply.github.com>
2024-11-23 03:45:03 -0500

    Menu hamburguesa

--- js/script.js
+++ js/script.js
@@ -1,15 +1,32 @@
-const menuToggle = document.getElementById('menu-toggle');
-const menu = document.getElementById('menu');
+const menuToggle = document.getElementById("menu-toggle");
+const menu = document.getElementById("menu");
+const nav = document.querySelector("nav");
 
 // Abre y cierra el menú al hacer clic en el botón de hamburguesa
-menuToggle.addEventListener('click', () => {
-  menu.classList.toggle('showing');
+menuToggle.addEventListener("click", () => {
+  menu.classList.toggle("showing");
+  if (nav.classList.contains("mostrarActiva")) {
+    nav.classList.remove("mostrarActiva");
+  } else {
+    nav.classList.add("mostrarActiva");
+  }
 });
 
 // Cierra el menú si se hace clic fuera de él
-document.addEventListener('click', (event) => {
+document.addEventListener("click", (event) => {
   // Verifica si el clic no fue en el menú o en el botón de hamburguesa
-  if (!menu.contains(event.target) && !menuToggle.contains(event.target) && menu.classList.contains('showing')) {
-    menu.classList.remove('showing');
+  if (
+    !menu.contains(event.target) &&
+    !menuToggle.contains(event.target) &&
+    menu.classList.contains("showing")
+  ) {
+    menu.classList.remove("showing");
+  }
+  if (
+    !nav.contains(event.target) &&
+    !menuToggle.contains(event.target) &&
+    nav.classList.contains("mostrarActiva")
+  ) {
+    nav.classList.remove("mostrarActiva");
   }
 });
