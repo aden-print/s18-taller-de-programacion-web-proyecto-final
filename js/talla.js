1258e654c1155a8520361258e4e46c30937fc36e
brandon01inga <14.2002brandoninga@gmail.com>
2024-11-23 01:02:22 -0500

    cambios modal talla

--- js/talla.js
+++ js/talla.js
@@ -1,4 +1,33 @@
-import { listarDatos } from "../supabase/operaciones.js";
+import { listarDatos, crearRegistro } from "../supabase/operaciones.js";
+
+const modal = document.querySelector(".modal-agregar");
+const mostrarModal = document.querySelector(".button__add");
+const cerrarModal = document.querySelector(".modal-close");
+const btnCancelar = document.querySelector(".btn-cancelar");
+const numerTalla = document.querySelector("#numero");
+const descripcionTalla = document.querySelector("#descripcion");
+const formAgregar = document.querySelector(".form-agregar");
+const btnAgregar = document.querySelector(".btn-agregar");
+const mensajeAgregar = document.querySelector(".mensaje-agregar");
+const data = { numero: "", descripcion: "" };
+
+mostrarModal.addEventListener("click", (e) => {
+  e.preventDefault();
+  limpiarFormulario();
+  modal.classList.add("modal-show");
+});
+
+cerrarModal.addEventListener("click", (e) => {
+  e.preventDefault();
+  limpiarFormulario();
+  modal.classList.remove("modal-show");
+});
+
+btnCancelar.addEventListener("click", (e) => {
+  e.preventDefault();
+  limpiarFormulario();
+  modal.classList.remove("modal-show");
+});
 
 async function cargarTallas() {
   const dataTallas = await listarDatos("talla", "idtalla", "*");
@@ -45,5 +74,42 @@ async function cargarTallas() {
 
   mostrarTallas();
 }
+
+function limpiarFormulario() {
+  numerTalla.value = "";
+  descripcionTalla.value = "";
+}
+numerTalla.addEventListener("input", leerInput);
+descripcionTalla.addEventListener("input", leerInput);
+
+function leerInput(e) {
+  data[e.target.id] = e.target.value;
+}
+
+formAgregar.addEventListener("submit", async function (evento) {
+  evento.preventDefault();
+  const { numero: nombre } = data;
+  if (nombre.trim() === "") {
+    alert("El campo Numero no puede estar vacio");
+    return;
+  }
+
+  btnAgregar.value = "Guardando...";
+  const respuesta = await crearRegistro("talla", data);
+  if (respuesta.error) {
+    alert("Error al agregar la talla");
+    mensajeAgregar.textContent = "Error al agregar la talla";
+    return;
+  }
+  btnAgregar.value = "Guardar";
+  mensajeAgregar.classList.remove("red");
+  mensajeAgregar.textContent = "Talla agregada correctamente";
+  setTimeout(() => {
+    modal.classList.remove("modal-show");
+    cargarTallas();
+    limpiarFormulario();
+    mensajeAgregar.textContent = "";
+  }, 4000);
+});
 
 cargarTallas();
