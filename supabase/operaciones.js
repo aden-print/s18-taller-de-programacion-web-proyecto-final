aee092f544302ca5099fa20f9f4e6dfb8d39ee4c
AngelChira <125518726+AngelChira@users.noreply.github.com>
2024-12-03 23:09:40 -0500

    Agregando operaciones para consultar el cubo de archivos

--- supabase/operaciones.js
+++ supabase/operaciones.js
@@ -8,6 +8,14 @@ export async function listarDatos(tabla, idTabla, campos) {
   return respuesta;
 }
 
+export async function obtenerRegistro(tabla, idColumna, idTabla) {
+  const respuesta = await supabaseClient
+    .from(tabla)
+    .select("*")
+    .eq(idColumna, idTabla);
+  return respuesta;
+}
+
 export async function crearRegistro(table, data) {
   const respuesta = await supabaseClient.from(table).insert(data);
   return respuesta;
@@ -31,5 +39,33 @@ export async function eliminar(table, idColumna, idTabla) {
 
 export async function consumir_funcion(nombre_funcion, parametros) {
   const respuesta = await supabaseClient.rpc(nombre_funcion, parametros);
+  return respuesta;
+}
+
+export async function subir_archivo(archivo) {
+  const respuesta = await supabaseClient.storage
+    .from("nombre_de_bucket")
+    .upload("nombre_de_archivo", archivo);
+  return respuesta;
+}
+
+export async function descargar_archivo(nombre_archivo) {
+  const respuesta = await supabaseClient.storage
+    .from("nombre_de_bucket")
+    .download(nombre_archivo);
+  return respuesta;
+}
+
+export async function eliminar_archivo(nombre_archivo) {
+  const respuesta = await supabaseClient.storage
+    .from("nombre_de_bucket")
+    .remove(nombre_archivo);
+  return respuesta;
+}
+
+export async function listar_archivos() {
+  const respuesta = await supabaseClient.storage
+    .from("nombre_de_bucket")
+    .list();
   return respuesta;
 }
