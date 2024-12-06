b564606eabb1208360b706da6e474a39f7a6fa3e
AngelChira <125518726+AngelChira@users.noreply.github.com>
2024-12-05 23:38:41 -0500

    Editar talla validación lista

--- js/editarTalla.js
+++ js/editarTalla.js
@@ -1,19 +1,25 @@
-import { obtenerRegistro, actualizar } from "../supabase/operaciones.js";
+import {
+  obtenerRegistro,
+  actualizar,
+  listarDatos,
+} from "../supabase/operaciones.js";
 
 const queryParams = window.location.search;
 const inputId = document.querySelector("#id");
 const inputTalla = document.querySelector("#numero");
+const inputDescripcion = document.querySelector("#descripcion");
 const formularioEditar = document.querySelector(".form-editar");
 const btnEditar = document.querySelector(".btn-editar");
 const mensajeEditar = document.querySelector(".mensaje-editar");
 
 const urlParams = new URLSearchParams(queryParams);
 const idtalla = urlParams.get("id");
+let datosTallas = [];
 
 async function cargarTallas() {
   const { data: talla, error } = await obtenerRegistro(
     "talla",
-    "idcatalla",
+    "idtalla",
     idtalla
   );
   if (error) {
@@ -21,7 +27,10 @@ async function cargarTallas() {
     return;
   }
   inputId.value = talla[0].idtalla;
-  inputTalla.value = talla[0].numero.trim();
+  inputTalla.value = talla[0].numero;
+  inputDescripcion.value = talla[0].descripcion
+    ? talla[0].descripcion.trim()
+    : "";
 }
 
 cargarTallas();
@@ -31,10 +40,32 @@ formularioEditar.addEventListener("submit", async (e) => {
   btnEditar.value = "Guardando...";
   btnEditar.disabled = true;
   const dataActualizar = {
-    nombre: inputTalla.value.trim(),
+    numero: parseInt(inputTalla.value.trim()),
+    descripcion: inputDescripcion.value.trim(),
   };
-  console.log(dataActualizar);
-  console.log(idt);
+
+  // Verificar si la talla ya existe
+  const tallaDuplicada = datosTallas.some(
+    (talla) =>
+      talla.numero === dataActualizar.numero &&
+      parseInt(talla.idtalla) !== parseInt(idtalla)
+  );
+
+  if (tallaDuplicada) {
+    mensajeEditar.textContent = "Error, ya existe esa talla.";
+    btnEditar.disabled = false;
+    btnEditar.value = "Guardar";
+    return;
+  }
+
+  if (dataActualizar.numero < 15 || dataActualizar.numero > 48) {
+    mensajeEditar.textContent =
+      "Error, el número de talla debe estar entre 15 y 48.";
+    btnEditar.disabled = false;
+    btnEditar.value = "Guardar";
+    return;
+  }
+
   const { error } = await actualizar(
     "talla",
     dataActualizar,
@@ -43,11 +74,12 @@ formularioEditar.addEventListener("submit", async (e) => {
   );
   if (error) {
     console.log(error);
-    if (error.code === "23505") {
-      mensajeEditar.textContent = "Error al actualizar, ya existe esa talla.";
-    }
     btnEditar.disabled = false;
     btnEditar.value = "Guardar";
+    if (error.code === "23505") {
+      mensajeEditar.textContent = "Error, ya existe esa talla.";
+      return;
+    }
     mensajeEditar.textContent = "Error al actualizar el registro";
     return;
   }
@@ -57,3 +89,17 @@ formularioEditar.addEventListener("submit", async (e) => {
     window.location.href = "talla.html";
   }, 4000);
 });
+
+async function obtenerTallas() {
+  const { data: tallas, error } = await listarDatos("talla", "idtalla", "*");
+  if (error) {
+    console.log("Error al cargar las tallas");
+    return;
+  }
+  datosTallas = tallas.map((talla) => ({
+    idtalla: talla.idtalla,
+    numero: talla.numero,
+  }));
+}
+
+obtenerTallas();
