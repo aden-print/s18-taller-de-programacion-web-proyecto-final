c362714f20d75b71c7b8f66e379d9c3fefcfea13
aden-print <180788966+aden-print@users.noreply.github.com>
2024-12-06 22:52:02 -0500

    arreglando menu amburguesa

--- js/script.js
+++ js/script.js
@@ -3,30 +3,16 @@ const menu = document.getElementById("menu");
 const nav = document.querySelector("nav");
 
 // Abre y cierra el menú al hacer clic en el botón de hamburguesa
-menuToggle.addEventListener("click", () => {
+menuToggle.addEventListener("click", (event) => {
+  event.stopPropagation(); // Evita cerrar el menú al hacer clic en el botón
   menu.classList.toggle("showing");
-  if (nav.classList.contains("mostrarActiva")) {
-    nav.classList.remove("mostrarActiva");
-  } else {
-    nav.classList.add("mostrarActiva");
-  }
+  nav.classList.toggle("mostrarActiva");
 });
 
 // Cierra el menú si se hace clic fuera de él
 document.addEventListener("click", (event) => {
-  // Verifica si el clic no fue en el menú o en el botón de hamburguesa
-  if (
-    !menu.contains(event.target) &&
-    !menuToggle.contains(event.target) &&
-    menu.classList.contains("showing")
-  ) {
+  if (!menu.contains(event.target) && !menuToggle.contains(event.target)) {
     menu.classList.remove("showing");
-  }
-  if (
-    !nav.contains(event.target) &&
-    !menuToggle.contains(event.target) &&
-    nav.classList.contains("mostrarActiva")
-  ) {
     nav.classList.remove("mostrarActiva");
   }
 });
